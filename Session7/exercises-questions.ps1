772f74d627ed953533c7d28ed8856deb469ae0bb
b.amiri <b.amiri@digikala.com>
2024-09-24 19:52:35 +0330

    Updated Session 7

---
+++ Session7/exercises-questions.ps1
@@ -0,0 +1,8 @@
+# write a powershell module called PSDemoEmployee, including below Cmdlets:
+New-Employee
+Get-Employee
+Set-Employee
+Remove-Employee
+Reset-EmployeeData
+
+# Cmdlets must have help.
